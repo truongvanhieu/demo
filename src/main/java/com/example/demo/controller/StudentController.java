746925aac676fd1e7bf3e4ced12d037a7df47dfa
HIEUTV <truongvanhieu2003@gmail.com>
2025-12-28 19:58:35 +0700

    add api all getallstudents

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +15,14 @@ public class StudentController {
     @GetMapping("/student")
     public Student getStudent() {
         return new Student(0, "Nguyễn Văn A", "a1@gmail.com");
+    }
+
+    @GetMapping("/getallstudents")
+    public List<Student> getAllStudents() {
+        List<Student> list = new ArrayList<>();
+        list.add(new Student(1, "Nguyễn Văn A", "a1@gmail.com"));
+        list.add(new Student(2, "Trần Thị B", "b2@gmail.com"));
+        list.add(new Student(3, "Lê Văn C", "c3@gmail.com"));
+        return list;
     }
 }
