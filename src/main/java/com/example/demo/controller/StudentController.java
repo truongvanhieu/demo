c9390ce816e65b06e2d7c4d8ed5807b4bf14ccbd
HIEUTV <truongvanhieu2003@gmail.com>
2025-12-29 23:42:18 +0700

    get all student and show table in file index.htm use bootsrap framework

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.example.demo.model.Student;
 
 @RestController
 @RequestMapping("/api/students")
+@CrossOrigin // cho phép frontend gọi
 public class StudentController {
 
     @Autowired
